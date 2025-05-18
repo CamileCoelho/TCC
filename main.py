1cf10e7b826b975d49f715221542217e447107d3
Jonatam Sturcio <bitcomanderbr@gmail.com>
2025-05-18 20:32:17 -0300

    Alterações necessárias para rodar o projeto.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
 from modelos.cnn import CNN
 import torch
 from torchvision import datasets
 from torchvision.transforms import v2
-import time
-
+import csv, os
 
 def define_transforms(height, width):
     data_transforms = {
@@ -21,26 +19,70 @@ def define_transforms(height, width):
     }
     return data_transforms
 
+# Troca o "." por "," para separar corretamente os decimais.
+def formatar_valor(valor):
+    if isinstance(valor, float):
+        return f"{valor:.6f}".replace('.', ',')
+    return valor
+
+def salvar_resultados_csv(execucoes, modelo, epocas, lr, wd, resultado, caminho='./resultados/resultados_treinamento.csv'):
+
+    #Separa o resultado em campos e os formata.
+    
+    acuracia = formatar_valor(resultado['accuracy'])
+    precisao = formatar_valor(resultado['precision'])
+    recall = formatar_valor(resultado['recall'])
+    f1_score = formatar_valor(resultado['f1'])
+    f_beta = formatar_valor(resultado['fbeta'])
+    loss = formatar_valor(resultado['loss'])
+    lr = formatar_valor(lr)
+    wd = formatar_valor(wd)
+
+    linha = [execucoes, modelo, epocas, lr, wd, acuracia, precisao, recall, f1_score, f_beta, loss]
+
+    # Cria o diretório caso não exista.
+    os.makedirs(os.path.dirname(caminho), exist_ok=True)
+
+    # Abre o arquivo para acrescentar uma linha
+    with open(caminho, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+
+        # Se o arquivo estiver vazio, escreve o cabeçalho
+        if file.tell() == 0:
+            writer.writerow(['Nº Execução','Modelo', 'Épocas', 'Learning Rate', 'Weight Decay', 'Acurácia', 'Precisão', 'Recall', 'F1-Score', 'Fbeta', 'Loss'])
+
+        writer.writerow(linha)
+
+    print(f"\nResultados salvos em {caminho}")
 
 def read_images(data_transforms):
-    train_data = datasets.ImageFolder('./data/resumido/train/',transform=data_transforms['train'])
-    validation_data = datasets.ImageFolder('./data/resumido/validation/',transform=data_transforms['test'])
-    test_data = datasets.ImageFolder('./data/resumido/test/',transform=data_transforms['test'])
+    train_data = datasets.ImageFolder('./data/train/',transform=data_transforms['train'])
+    validation_data = datasets.ImageFolder('./data/dev/',transform=data_transforms['test'])
+    test_data = datasets.ImageFolder('./data/test/',transform=data_transforms['test'])
     return train_data, validation_data, test_data
 
 if __name__ == '__main__':
     data_transforms = define_transforms(224,224)
     train_data, validation_data, test_data = read_images(data_transforms)
     cnn = CNN(train_data, validation_data, test_data,8)
-#Esta é a parte do código que deve ser atualizada e distribuída
-    replicacoes = 10
-    model_names=['Alexnet']
-    epochs = [10]
-    learning_rates = [0.001]
-    weight_decays = [0]
-    inicio = time.time()
-    acc_media, rep_max = cnn.create_and_train_cnn(model_names[0],epochs[0],learning_rates[0],weight_decays[0],replicacoes)
-    fim = time.time()
-    duracao = fim - inicio
-    print(f"{model_names[0]}-{epochs[0]}-{learning_rates[0]}-{weight_decays[0]}-Acurácia média: {acc_media} - Melhor replicação: {rep_max} - Tempo:{duracao}")
+
+    execucoes = 10 # Numero de vezes que o treino será executado.
+    #replicacoes = 10 ||| Não tenho certeza quanto a esse parametro. / Verificar com o Castello sobre. / Quase certeza que nesse contexto não será usado.
+    model_names=['VGG19']
+    epochs = [50]
+    learning_rates = [0.0001]
+    weight_decays = [0.01]
+
+    for i in range(1, execucoes+1):
+        print(f"Execução Nº: {i}")
+        resultado = cnn.create_and_train_cnn(model_names[0],epochs[0],learning_rates[0],weight_decays[0])
+
+        salvar_resultados_csv(
+        execucoes,
+        modelo=model_names[0],
+        epocas=epochs[0],
+        lr=learning_rates[0],
+        wd=weight_decays[0],
+        resultado=resultado
+)
     
--- modelos/cnn.py
+++ modelos/cnn.py
@@ -3,50 +3,63 @@ import torch
 from torch import nn, optim
 from torch.utils import data
 from torchvision import models
-
+from sklearn.metrics import recall_score, f1_score, fbeta_score, classification_report, precision_score
 
 class CNN:
     def __init__(self, train_data, validation_data, test_data, batch_size):
         self.train_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
         self.validation_loader = data.DataLoader(validation_data, batch_size=batch_size, shuffle=False)
         self.test_loader = data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
-        self.device = torch.device("cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def create_and_train_cnn(self, model_name, num_epochs, learning_rate, weight_decay, replicacoes):
-        soma = 0
-        acc_max = 0
-        for i in range(0,replicacoes):
-            model = self.create_model(model_name)
-            optimizerSGD = self.create_optimizer(model,learning_rate, weight_decay)
-            criterionCEL = self.create_criterion()
-            self.train_model(model, self.train_loader, optimizerSGD, criterionCEL, model_name, num_epochs, learning_rate, weight_decay, i) 
-            acc = self.evaluate_model(model, self.validation_loader)
-            soma = soma + acc
-            if acc > acc_max:
-                acc_max = acc
-                iter_acc_max = i
-        return soma / replicacoes, iter_acc_max
+
+    def create_and_train_cnn(self, model_name, num_epochs, learning_rate, weight_decay):        
+        model = self.create_model(model_name)
+        optimizerSGD = self.create_optimizer(model,learning_rate, weight_decay)
+        criterionCEL = self.create_criterion()
+        self.train_model(model, self.train_loader, optimizerSGD, criterionCEL, num_epochs) 
+        metrics = self.evaluate_model(model, self.validation_loader)
+        return metrics
+
+    # Método original, deixei para caso seja necessário voltar. / Talvez necessário se precisar das replicações.
+
+    # def create_and_train_cnn(self, model_name, num_epochs, learning_rate, weight_decay, replicacoes):
+    #     soma = 0
+    #     acc_max = 0
+    #     for i in range(0,replicacoes):
+    #         model = self.create_model(model_name)
+    #         optimizerSGD = self.create_optimizer(model,learning_rate, weight_decay)
+    #         criterionCEL = self.create_criterion()
+    #         self.train_model(model, self.train_loader, optimizerSGD, criterionCEL, model_name, num_epochs, learning_rate, weight_decay, i) 
+    #         acc = self.evaluate_model(model, self.validation_loader)
+    #         soma = soma + acc
+    #         if acc > acc_max:
+    #             acc_max = acc
+    #             iter_acc_max = i
+    #     return soma / replicacoes, iter_acc_max
         
     
     def create_model(self, model_name):
-        if (model_name=='VGG11'):
-            model = models.vgg11(weights='DEFAULT')  
+        if model_name == 'VGG19':
+            model = models.vgg19(weights='DEFAULT')
             for param in model.parameters():
                 param.requires_grad = False
-            model.classifier[6] = nn.Linear(model.classifier[6].in_features,2)
+            model.classifier[6] = nn.Linear(model.classifier[6].in_features, 2)
             return model
-        elif (model_name=='Alexnet'):
-            model = models.alexnet(weights='DEFAULT')  
-            for param in model.parameters():
-                param.requires_grad = False
-            model.classifier[6] = nn.Linear(model.classifier[6].in_features,2)
-            return model
-        else: # 'if (model_name=='MobilenetV3Large' ou qualquer outra coisa para não dar erro)
-            model = models.mobilenet_v3_large(weights='DEFAULT')  
-            for param in model.parameters():
-                param.requires_grad = False
-            model.classifier[3] = nn.Linear(model.classifier[3].in_features,2)
-            return model
+        
+        ## Metodo pode ser utilizado para ensembles futuramente, manter estrutura.
+        # elif (model_name=='VGG11'):
+        #     model = models.vgg11(weights='DEFAULT')  
+        #     for param in model.parameters():
+        #         param.requires_grad = False
+        #     model.classifier[6] = nn.Linear(model.classifier[6].in_features,2)
+        #     return model
+        # elif (model_name=='Alexnet'):
+        #     model = models.alexnet(weights='DEFAULT')  
+        #     for param in model.parameters():
+        #         param.requires_grad = False
+        #     model.classifier[6] = nn.Linear(model.classifier[6].in_features,2)
+        #     return model
    
     def create_optimizer(self, model, learning_rate, weight_decay):
         update = []
@@ -60,16 +73,26 @@ class CNN:
         criterionCEL = nn.CrossEntropyLoss()
         return criterionCEL
 
-    def train_model(self, model, train_loader, optimizer, criterion, model_name, num_epochs, learning_rate, weight_decay, replicacao): 
+    def train_model(self, model, train_loader, optimizer, criterion, num_epochs): 
         model.to(self.device)
-        min_loss = 100
-        e_measures = []
         for i in (range(1,num_epochs+1)):
-            train_loss = self.train_epoch(model, train_loader, optimizer, criterion)
-            if (train_loss < min_loss):
-                min_loss = train_loss
-                nome_arquivo = f"./modelos/{model_name}_{num_epochs}_{learning_rate}_{weight_decay}_{replicacao}.pth"
-                torch.save(model.state_dict(), nome_arquivo)
+            print(f"\tTreinando epoca: {i}", end='\r')
+            self.train_epoch(model, train_loader, optimizer, criterion)
+
+
+    #Metodo de treino anterior, comentei por ter modificado e removido a parte onde ele salva o modelo. Posteriormente pode ser usado.
+
+    # def train_model(self, model, train_loader, optimizer, criterion, model_name, num_epochs, learning_rate, weight_decay, replicacao): 
+    #     model.to(self.device)
+    #     min_loss = 100
+    #     e_measures = []
+    #     for i in (range(1,num_epochs+1)):
+    #         train_loss = self.train_epoch(model, train_loader, optimizer, criterion)
+
+    #         if (train_loss < min_loss):
+    #             min_loss = train_loss
+    #             nome_arquivo = f"./modelos/{model_name}_{num_epochs}_{learning_rate}_{weight_decay}_{replicacao}.pth"
+    #             torch.save(model.state_dict(), nome_arquivo)
 
     def train_epoch(self,model, trainLoader, optimizer, criterion):
         model.train()
@@ -86,16 +109,40 @@ class CNN:
         model.eval()
         return np.mean(losses)
 
-    def evaluate_model(self,model, loader):
-        total = 0
-        correct = 0
-        for X, y in loader:
-            X, y = X.to(self.device), y.to(self.device)
-            output = model(X)
-            _, y_pred = torch.max(output, 1)
-            total += len(y)
-            correct += (y_pred == y).sum().cpu().data.numpy()
-        acc = correct/total
-        return acc
+    def evaluate_model(self, model, loader):
+        model.to(self.device)
+        model.eval()
+
+        y_true = []
+        y_pred_list = []
+        total_loss = 0
+        criterion = nn.CrossEntropyLoss()
+
+        with torch.no_grad():
+            for X, y in loader:
+                X, y = X.to(self.device), y.to(self.device)
+                outputs = model(X)
+                loss = criterion(outputs, y)
+                total_loss += loss.item()
+
+                _, preds = torch.max(outputs, 1)
+                y_true.extend(y.cpu().numpy())
+                y_pred_list.extend(preds.cpu().numpy())
+
+        # Calcula métricas
+        acc = np.mean(np.array(y_true) == np.array(y_pred_list))
+        precision = precision_score(y_true, y_pred_list, average='macro') 
+        recall = recall_score(y_true, y_pred_list, average='macro')
+        f1 = f1_score(y_true, y_pred_list, average='macro')
+        fbeta = fbeta_score(y_true, y_pred_list, beta=0.5, average='macro')
+        avg_loss = total_loss / len(loader)
+        return {
+            "accuracy": acc,
+            "precision": precision,
+            "recall": recall,
+            "f1": f1,
+            "fbeta": fbeta,
+            "loss": avg_loss
+        }
 
 
